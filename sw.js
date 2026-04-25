dc851f794435d78d580000ced29c1233e0b7586c
Ayoub EL WAMY <Ayoubelwamy10121964@gmail.com>
2026-04-25 21:33:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -12,6 +12,7 @@ const STATIC_URLS = [
   '/js/Stock.js',
   '/js/UIController.js',
   '/js/Constants.js',
+  '/js/Utility.js',
   '/manifest.json',
   'https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=JetBrains+Mono:wght@300;400;500;700&display=swap',
 ];
